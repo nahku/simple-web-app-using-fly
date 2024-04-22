6f6d18eec262036596823c3c7918c24c941f5fca
Nahku Saidy <nsaidy@t-online.de>
2024-04-22 15:35:28 +0100

    ci: add build step

--- src/test/java/com/oocode/AnswererTest.java
+++ src/test/java/com/oocode/AnswererTest.java
@@ -10,6 +10,6 @@ public class AnswererTest {
     public void canAnswerMyName() {
         var contents = new Answerer().answerFor("What is your name?");
 
-        assertThat(contents, equalTo("Your ame"));
+        assertThat(contents, equalTo("Your Name"));
     }
 }
